eb377b1af4d9bfe8b26b892039ecc2acc609f162
Khoawawa <atom08072004@gmail.com>
2026-09-18 13:30:29 +0700

    [KhoaNA] update main.sh

--- main.sh
+++ main.sh
@@ -5,15 +5,15 @@ BRANCH=${1:-main}
 PROFILE=${2:-dev}
 CONFIG=${3:-configs/config.yaml}
 
-if [ ! -d DeepLearning_Framework ]; then
-    git clone -b "$BRANCH" https://github.com/Khoawawa/DeepLearning_Framework.git
-    cd DeepLearning_Framework
-else
-    cd DeepLearning_Framework
-    git fetch origin
-    git checkout "$BRANCH"
-    git reset --hard origin/"$BRANCH"
-fi
+# if [ ! -d DeepLearning_Framework ]; then
+#     git clone -b "$BRANCH" https://github.com/Khoawawa/DeepLearning_Framework.git
+#     cd DeepLearning_Framework
+# else
+#     cd DeepLearning_Framework
+#     git fetch origin
+#     git checkout "$BRANCH"
+#     git reset --hard origin/"$BRANCH"
+# fi
 
 pip install -q -r requirements.txt
 
